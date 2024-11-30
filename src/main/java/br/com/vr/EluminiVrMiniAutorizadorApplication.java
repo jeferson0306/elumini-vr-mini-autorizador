2c515c8722255c057c8127eb73a18b5ea5cf584c
Jeferson Siqueira <jeferson0306@msn.com>
2024-11-30 09:05:36 -0300

    primeiro commit com as configuracoes iniciais do projeto

---
+++ src/main/java/br/com/vr/EluminiVrMiniAutorizadorApplication.java
@@ -0,0 +1,13 @@
+package br.com.vr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EluminiVrMiniAutorizadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EluminiVrMiniAutorizadorApplication.class, args);
+	}
+
+}
